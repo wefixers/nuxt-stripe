df22c90f6bde232bb901ea08267360e93e2bcffe
lucacicada <100947658+lucacicada@users.noreply.github.com>
2023-07-04 21:12:43 +0200

    chore: use an empty string as env default value

--- playground/env.ts
+++ playground/env.ts
@@ -3,10 +3,10 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    STRIPE_WEBHOOK_SECRET: z.string(),
-    STRIPE_CLIENT_SECRET: z.string(),
+    STRIPE_WEBHOOK_SECRET: z.string().default(''),
+    STRIPE_CLIENT_SECRET: z.string().default(''),
   },
   client: {
-    NUXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
+    NUXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().default(''),
   },
 })
