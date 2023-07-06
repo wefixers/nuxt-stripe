3ecc7657e5a0b341f505817996726af7a41b4b08
lucacicada <100947658+lucacicada@users.noreply.github.com>
2023-07-06 09:35:17 +0200

    docs: add plugin jsdoc with examples

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -3,19 +3,14 @@ import { loadStripe } from '@stripe/stripe-js/pure'
 
 import type { ModulePublicRuntimeConfig } from '../module'
 
+import type { Plugin } from '#app'
 import { defineNuxtPlugin, useRuntimeConfig } from '#app'
 
-/**
- * Create a {@link Stripe} Client instance.
- *
- * ### Note:
- * In SSR this method return null, as the client library cannot be loaded.
- */
-async function createClient(options?: StripeConstructorOptions, overridePublishableKey?: string): Promise<Stripe> {
-  const { publishableKey, client }: ModulePublicRuntimeConfig = useRuntimeConfig().public.stripe
+async function createClient(options?: StripeConstructorOptions, publishableKey?: string): Promise<Stripe> {
+  const stripeRuntimeConfig: ModulePublicRuntimeConfig | undefined = useRuntimeConfig().public.stripe
 
-  const stripe = await loadStripe(overridePublishableKey || publishableKey, {
-    ...client,
+  const stripe = await loadStripe(publishableKey || stripeRuntimeConfig?.publishableKey || '', {
+    ...stripeRuntimeConfig?.client,
     ...options,
   })
 
@@ -23,7 +18,34 @@ async function createClient(options?: StripeConstructorOptions, overridePublisha
   return stripe!
 }
 
-export default defineNuxtPlugin(() => {
+/**
+ * Represents a Nuxt Stripe plugin.
+ */
+export interface StripePlugin {
+  /**
+   * Create a new {@link Stripe} Client instance.
+   *
+   * ### Note:
+   * In SSR this method return null, as the client library cannot be loaded.
+   */
+  createClient: (options?: StripeConstructorOptions, overridePublishableKey?: string) => Promise<Stripe>
+}
+
+/**
+ * The Nuxt Stripe plugin.
+ *
+ * ### Example:
+ * ```ts
+ * const { $stripe } = useNuxtApp()
+ *
+ * const stripe = await $stripe.createClient()
+ *
+ * await stripe.redirectToCheckout({
+ *   sessionId: session.id,
+ * })
+ * ```
+ */
+const plugin: Plugin<{ stripe: StripePlugin }> = defineNuxtPlugin(() => {
   return {
     provide: {
       stripe: {
@@ -32,3 +54,5 @@ export default defineNuxtPlugin(() => {
     },
   }
 })
+
+export default plugin
