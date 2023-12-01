ebf448998160feb9dc771357d36b2079a39f2639
lucacicada <100947658+lucacicada@users.noreply.github.com>
2023-12-01 19:36:11 +0100

    chore: Remove nuxt plugin from playground

--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts">
-import { useNuxtApp } from '#app'
 import { useStripe } from '#imports'
 
 const { createClient: createStripeClient } = useStripe()
-const { $stripe } = useNuxtApp()
 
 async function checkout() {
-  const session = await $fetch('/api/createStripeSession', {
-    method: 'POST',
-  })
-
-  const stripe = await $stripe.createClient()
-  await stripe.redirectToCheckout({
-    sessionId: session.id,
-  })
-}
-
-async function checkoutWithComposables() {
   const session = await $fetch('/api/createStripeSession', {
     method: 'POST',
   })
