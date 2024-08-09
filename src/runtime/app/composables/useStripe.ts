02b48b11767d380dd4e3544fd526576eee5305ff
lucacicada <100947658+lucacicada@users.noreply.github.com>
2024-08-09 11:57:43 +0200

    Do not instance elements in useStripe

--- src/runtime/app/composables/useStripe.ts
+++ src/runtime/app/composables/useStripe.ts
@@ -1,17 +1,15 @@
-import type { StripeConstructorOptions, StripeElements } from '@stripe/stripe-js'
-import { loadStripe } from '@stripe/stripe-js/pure'
-
+import type { Ref } from 'vue'
+import { unref } from 'vue'
 import { computedAsync } from '@vueuse/core'
 
-import type { Ref } from 'vue'
-import { provide, shallowRef, unref } from 'vue'
+import type { Stripe, StripeConstructorOptions } from '@stripe/stripe-js'
+import { loadStripe } from '@stripe/stripe-js/pure'
 
-import type { StripeContext } from '../../types'
 import { useRuntimeConfig } from '#imports'
 
 type MaybeRef<T> = T | Ref<T>
 
-interface StripeOptions extends StripeConstructorOptions {
+export interface UseStripeOptions extends StripeConstructorOptions {
   /**
    * Override the runtime configured publishable key.
    */
@@ -23,25 +21,18 @@ interface StripeOptions extends StripeConstructorOptions {
  *
  * ### Example:
  * ```ts
- * const { stripe } = useStripe()
+ * const stripe = useStripe()
  *
  * await stripe!.redirectToCheckout({
  *   sessionId: session.id,
  * })
  * ```
  */
-export function useStripe(options?: MaybeRef<StripeOptions>) {
+export function useStripe(options?: MaybeRef<UseStripeOptions>): Ref<Stripe | null> {
   const runtimeConfig = useRuntimeConfig()
 
-  /**
-   * Keep track of the Stripe Elements instance.
-   * We need this here so we can return a ref as part of this composable.
-   */
-  const stripeElements = shallowRef<StripeElements>()
-
-  // I really do not like computedAsync, it might get
-  // re-evaluated when useFetch() is re-fetched even tho the value is the same
-  const stripeInstance = computedAsync(() => {
+  // I really do not like computedAsync but it does the job
+  return computedAsync(() => {
     const opts = unref(options)
     const pk = opts?.publishableKey || runtimeConfig.public.stripe?.publishableKey
 
@@ -54,16 +45,4 @@ export function useStripe(options?: MaybeRef<StripeOptions>) {
       stripeAccount: opts?.stripeAccount || runtimeConfig.public.stripe?.options?.stripeAccount || undefined,
     })
   })
-
-  // Provide a mutable context Ref<> for child components
-  provide<StripeContext>('nuxt-stripe-context', {
-    stripe: stripeInstance,
-    elements: stripeElements,
-  })
-
-  return {
-    // TODO: consider using readonly() here as we have a computed
-    stripe: stripeInstance,
-    elements: stripeElements,
-  }
 }
