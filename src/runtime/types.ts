1c61610b9bfccabc67d585e3e471713d69785f3b
lucacicada <100947658+lucacicada@users.noreply.github.com>
2024-08-10 10:13:07 +0200

    Remove unused StripeContext interface

--- src/runtime/types.ts
+++
@@ -1,7 +0,0 @@
-import type { Stripe, StripeElements } from '@stripe/stripe-js'
-import type { Ref } from 'vue'
-
-export interface StripeContext {
-  stripe: Ref<Stripe | null>
-  elements: Ref<StripeElements | undefined>
-}
