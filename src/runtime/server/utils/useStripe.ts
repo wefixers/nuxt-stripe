56bb934784b027e36ddd866cb4631f0ec165300c
lucacicada <100947658+lucacicada@users.noreply.github.com>
2024-09-02 08:53:56 +0200

    Refactor useStripe to use default fetch API as HTTP client

--- src/runtime/server/utils/useStripe.ts
+++ src/runtime/server/utils/useStripe.ts
@@ -6,7 +6,9 @@ let _stripe: Stripe | undefined
 /**
  * Gets the {@link Stripe} runtime instance.
  *
- * Note: this method is intended to be used in the server-side only.
+ * This instance is created only once and recycled throughout the lifespan of the application.
+ *
+ * Note: this method is intended to be used the server-side only.
  *
  * If `fetch` is available in the global scope, it will be used as the default HTTP client.
  */
@@ -17,12 +19,22 @@ export function useStripe(): Stripe {
 
   const { stripe: { secret, options = {} } } = useRuntimeConfig()
 
-  // // Use the default fetch API as the HTTP client if it is available
-  // if (!options.httpClient && (globalThis as any).fetch) {
-  //   options.httpClient = Stripe.createFetchHttpClient()
-  // }
+  // see: https://nuxt.com/docs/guide/going-further/runtime-config#reading
+  // On server-side, the entire runtime config is available, but it is read-only to avoid context sharing.
+  // Copy the options to avoid mutating the runtime config.
+  const config: Stripe.StripeConfig = {
+    ...options,
+  }
 
-  _stripe = new Stripe(secret, options)
+  // Note: The fetch api is pretty standard across major runtimes.
+  // Setting it here will fix alot of issues with non Node runtimes.
+
+  // Use the default fetch API as the HTTP client when fetch it is available globally.
+  if (!config.httpClient && (globalThis as any).fetch) {
+    config.httpClient = Stripe.createFetchHttpClient()
+  }
+
+  _stripe = new Stripe(secret, config)
 
   return _stripe
 }
