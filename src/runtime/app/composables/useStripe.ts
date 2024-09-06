56af9ade15155d7da2a5b2b1c4efe94164a3b4a1
lucacicada <100947658+lucacicada@users.noreply.github.com>
2024-09-06 14:59:21 +0200

    Do not trigger unnecessary Stripe ref update

--- src/runtime/app/composables/useStripe.ts
+++ src/runtime/app/composables/useStripe.ts
@@ -1,6 +1,5 @@
 import type { Ref } from 'vue'
-import { provide, shallowRef, toRaw, unref, watch } from 'vue'
-import type { MaybeRef } from '@vueuse/core'
+import { computed, provide, shallowRef, toRaw, unref, watch } from 'vue'
 
 import type { Stripe, StripeConstructorOptions } from '@stripe/stripe-js'
 import { loadStripe } from '@stripe/stripe-js/pure'
@@ -8,6 +7,7 @@ import { loadStripe } from '@stripe/stripe-js/pure'
 import type { StripeContext } from '../types'
 import { useRuntimeConfig } from '#imports'
 
+type MaybeRef<T> = T | Ref<T>
 type Optional<T> = MaybeRef<T | null | undefined>
 
 export interface UseStripeOptions extends StripeConstructorOptions {
@@ -30,20 +30,54 @@ export interface UseStripeOptions extends StripeConstructorOptions {
  * ```
  */
 export function useStripe(options?: Optional<UseStripeOptions>): Ref<Stripe | null> {
-  const runtimeConfig = useRuntimeConfig()
+  const stripeConfig = useRuntimeConfig().public.stripe
 
   /**
    * The actual Stripe instance.
    */
   const stripe = shallowRef<Stripe | null>(null)
 
-  const stop = watch(() => options, async (optionsRef) => {
-    if (stripe.value) {
-      return
+  /**
+   * // The options to initialize the Stripe instance with.
+   */
+  const stripeOptions = computed<UseStripeOptions>(() => {
+    const rawOptions = {
+      // Merge runtime config options with the provided options
+      ...stripeConfig?.options,
+      ...toRaw(unref(options)),
     }
 
-    const options = toRaw(unref(optionsRef))
-    const publishableKey = options?.publishableKey || runtimeConfig.public.stripe?.publishableKey
+    rawOptions.publishableKey ||= stripeConfig?.publishableKey
+
+    return rawOptions
+  })
+
+  /**
+   * An hash key to determine if the Stripe instance should be reloaded.
+   *
+   * This hash is necessary, otherwise we run the risk of invalidating the Stripe instance,
+   * even when the options haven't changed.
+   *
+   * We need to avoid triggering a ref update since components will re-render flashing the UI.
+   */
+  const hashKey = computed(() => {
+    return `${
+      stripeOptions.value.publishableKey || ''
+    }-${
+      stripeOptions.value.apiVersion || ''
+    }-${
+      stripeOptions.value.apiVersion || ''
+    }-${
+      stripeOptions.value.locale || ''
+    }-${
+      // Sort the betas to ensure the hash is consistent
+      // Copy the array to avoid mutating the original
+      stripeOptions.value.betas?.filter(Boolean).map(x => String(x)).sort().join(',') || ''
+    }`
+  })
+
+  watch(hashKey, async () => {
+    const { publishableKey, ...options } = stripeOptions.value
 
     // No publishable key
     if (!publishableKey) {
@@ -51,20 +85,16 @@ export function useStripe(options?: Optional<UseStripeOptions>): Ref<Stripe | nu
     }
 
     try {
-      stripe.value = await loadStripe(publishableKey, {
-        // Merge runtime config options with the provided options
-        ...runtimeConfig.public.stripe?.options,
-        ...options,
-      })
-
-      stop()
+      stripe.value = await loadStripe(
+        publishableKey,
+        options,
+      )
     }
     catch {
       //
     }
   }, {
     immediate: true,
-    deep: true,
   })
 
   // Provide the Stripe instance to children components and composables
